eb2f1a33760da6d74b6a92a199452cf1e33600b3
Dr. Junaid ur Rehman <junaid_572@hotmail.com>
2022-03-11 12:21:00 +0900

    Update oqc_helper.py

--- oqc_helper.py
+++ oqc_helper.py
@@ -22,19 +22,21 @@ def Rz(th):
 
 
 def seq_to_matrix(g_s, a_s):
+    g_s = g_s[::-1]
+    a_s = a_s[::-1]
     mat = np.matrix(np.eye(2), dtype=complex)
     for gate, angle in zip(g_s, a_s):
         if gate == "X":
-            mat *= Rx(angle)
+            mat = Rx(angle)*mat
         elif gate == "Y":
-            mat *= Ry(angle)
+            mat = Ry(angle)*mat
         elif gate == "Z":
-            mat *= Rz(angle)
+            mat = Rz(angle)*mat
     
     if np.abs(mat[0, 0]) > 0.1:
         mat *= np.exp(-1j * np.angle(mat[0, 0]))  # fixing the phase of first entry to zero
     else:
-        mat *= np.exp(-1j * np.angle(mat[0, 1]))  # fixing the phase of first entry to zero
+        mat *= np.exp(-1j * np.angle(mat[0, 1]))  # fixing the phase of second (0, 1) entry to zero
     mat = np.around(mat, decimals=7)  # for cleaner results
 
     return mat
